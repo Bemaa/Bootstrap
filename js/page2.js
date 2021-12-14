76c532fada18312728dd84664e5025ae8279579b
Benjamin Madetoja <benjaminmadetoja@gmail.com>
2021-12-14 20:07:56 +0200

    Ensimmäistä ja toista sivua aloiteltu.

---
+++ js/page2.js
@@ -0,0 +1,25 @@
+//mouse hover gif functio
+
+//ensimmäiselle kuvalle
+    function setNewImage1(){
+    document.getElementById("pic01").src= "../Vids/essential03.gif";
+    }
+    function setOldImage1(){
+    document.getElementById("pic01").src= "../Photos/img02.jpg";
+    }
+
+//toiselle kuvalle
+    function setNewImage2(){
+    document.getElementById("pic02").src= "../Vids/essential04.gif";
+    }
+    function setOldImage2(){
+    document.getElementById("pic02").src= "../Photos/img01.jpg";
+    }
+
+//kolmannelle kuvalle
+    function setNewImage3(){
+    document.getElementById("pic03").src= "../Vids/essential05.gif";
+    }
+    function setOldImage3(){
+    document.getElementById("pic03").src= "../Photos/img03.jpg";
+    }
