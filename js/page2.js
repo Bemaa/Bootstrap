bc6953df2aeb86b8751e22c412981728c0db8445
Benjamin Madetoja <benjaminmadetoja@gmail.com>
2021-12-18 20:00:40 +0200

    Paljon tekstejä ja elementtejä paranneltu

--- js/page2.js
+++ js/page2.js
@@ -1,25 +1,33 @@
 //mouse hover gif functio
 
 //ensimmäiselle kuvalle
-    function setNewImage1(){
+    function setVideo1(){
     document.getElementById("pic01").src= "../Vids/essential03.gif";
     }
-    function setOldImage1(){
+    function setImage1(){
     document.getElementById("pic01").src= "../Photos/img02.jpg";
     }
 
 //toiselle kuvalle
-    function setNewImage2(){
+    function setVideo2(){
     document.getElementById("pic02").src= "../Vids/essential04.gif";
     }
-    function setOldImage2(){
+    function setImage2(){
     document.getElementById("pic02").src= "../Photos/img01.jpg";
     }
 
 //kolmannelle kuvalle
-    function setNewImage3(){
+    function setVideo3(){
     document.getElementById("pic03").src= "../Vids/essential05.gif";
     }
-    function setOldImage3(){
+    function setImage3(){
     document.getElementById("pic03").src= "../Photos/img03.jpg";
     }
+
+//neljännelle kuvalle
+    function setVideo4(){
+        document.getElementById("bottomVid").src= "../Vids/gifpage2.gif";
+        }
+        function setImage4(){
+        document.getElementById("bottomVid").src= "../Photos/still01.jpg";
+        }
